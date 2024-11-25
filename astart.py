767267ce5818ca14a34363f8f9bc0946f70ca65d
Jaroslav246 <slav4nko@gmail.com>
2024-11-25 09:59:12 +0200

    k

--- astart.py
+++ astart.py
@@ -4,12 +4,34 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print("Datubāze ir izveidota!") 
 
-conn.execute('''CREATE TABLE COMPANY
+cursor=conn.execute("SELECT * from COMPANY")
+for ieraksts in cursor:
+    print("ID= ", ieraksts[0])
+    print("NAME= ", ieraksts[1])
+    print("ADDRESS= ", ieraksts[3])
+    print("AGE= ", ieraksts[2])
+    print("SALARY= ", ieraksts[4], "\n")
+ 
+print("Dati ir veiksmīgi nolasīti!")
+
+
+'''conn.execute(CREATE TABLE COMPANY
              (ID INT PRIMATY KEY  NOT NULL,
              NAME           TEXT NOT NULL,
              AGE            INT  NOT NULL,
              ADDRESS        CHAR(50),
-             SALARY         REAL);''')
-print("Tabula ir izveidota!")
+             SALARY         REAL);)
+print("Tabula ir izveidota!")'''
 
+'''conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+      VALUES (1, 'PAUL', 32, 'California', 20000.0 )")
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+      VALUES (2, 'Alex', 25, 'Latvia', 15000.0 )")
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.0 )")
+conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) \
+      VALUES (4, 'Mark', 25, 'Germany', 65000.0 )")
+
+conn.commit()
+print("Ieraksti ir izveidoti")'''
 conn.close()
