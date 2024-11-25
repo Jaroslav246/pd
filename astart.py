0f86c35401551d8753bae789ffb12a26018d61f1
Jaroslav246 <slav4nko@gmail.com>
2024-11-25 09:03:02 +0200

    s

---
+++ astart.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+print("Datubāze ir izveidota!") 
+
+conn.execute('''CREATE TABLE COMPANY
+             (ID INT PRIMATY KEY  NOT NULL,
+             NAME           TEXT NOT NULL,
+             AGE            INT  NOT NULL,
+             ADDRESS        CHAR(50),
+             SALARY         REAL);''')
+print("Tabula ir izveidota!")
+
+conn.close()
--- sqlites.py
+++ sqlites.py
@@ -1,13 +1,44 @@
 import sqlite3
 
 
-conn = sqlite3.connect('Interneta_veikals.db')
+conn = sqlite3.connect('rieksti.db')
 cursor = conn.cursor()
 
 
 
 
-cursor.execute('SELECT * FROM Pasutiijumi')
+cursor.execute('''
+SELECT      
+     vards, datums, nosaukums, cena, daudzums
+            FROM
+               Detalas
+            Join
+               pasutijums on Detalas.pasutijuma_id = pasutijums.pasutijuma_id
+            Join
+               Klients on Klients.klienta_id = pasutijums.klienta_id
+            Join
+               produkts on produkts.produkts_id = Detalas.produkta_id
+
+
+
+
+
+
+''')
+
+
+produkts_id = int(input("Ievadi produkta ID: "))
+nosaukums = str(input("Ievadi produkta nosaukuma: "))
+cena = float(input("Ievadi produkta cenu: "))
+noliktava = bool(input("Ievadi noliktavas statusu (piemēram, 'ir' vai 'nav'): "))
+
+cursor.exucute('''
+INSERT INTO Produkti (produkts_id, nosaukums, cena, noliktava
+VALUES (?, ?, ?, ?)
+''', (produkts_id, nosaukums, cena, noliktava))
+
+print("Produkts pievienots!") 
+
 orders = cursor.fetchall()
 print("Pasūtījumi:")
 for order in orders:
